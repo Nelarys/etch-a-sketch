31a4054a54f802673e2fa3468417398e3f66d082
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-25 19:27:25 +0200

    feat: Add function that creates grid of arbitrary size
    This way we can later create functionality to change the size of the grid based on user input

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const container = document.querySelector("#container");
+function createGrid(size) {
+  const container = document.querySelector("#container");
+  const gridElementSize = `${600 / size - 2}px`;
 
-for (let i = 0; i < 16 * 16; i++) {
-  const gridElement = document.createElement("div");
-  gridElement.classList.add("grid-element");
-  container.appendChild(gridElement);
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("grid-element");
+    gridElement.addEventListener("mouseover", () => {
+      gridElement.classList.add("colored");
+    });
+    gridElement.style.width = gridElementSize;
+    gridElement.style.height = gridElementSize;
+
+    container.appendChild(gridElement);
+  }
+
+  const gridElements = document.querySelectorAll(".grid-element");
+  gridElements.forEach((gridElement) => {
+    console.log(gridElementSize);
+    // gridElement.style.cssText = `width: ${gridElementSize}; height: ${gridElementSize};`;
+    gridElement.style.width = gridElementSize;
+    gridElement.style.height = gridElementSize;
+  });
 }
 
-const gridElements = document.querySelectorAll(".grid-element");
-gridElements.forEach((gridElement) => {
-  gridElement.addEventListener("mouseover", () => {
-    gridElement.classList.add("colored");
-  });
-});
+createGrid(30);
