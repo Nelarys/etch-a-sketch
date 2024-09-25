4b802b583159f9f52f0d27f83389205d911491f0
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-25 18:49:11 +0200

    feat: Add cursor leaves trace effect

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ for (let i = 0; i < 16 * 16; i++) {
   gridElement.classList.add("grid-element");
   container.appendChild(gridElement);
 }
+
+const gridElements = document.querySelectorAll(".grid-element");
+gridElements.forEach((gridElement) => {
+  gridElement.addEventListener("mouseover", () => {
+    gridElement.classList.add("colored");
+  });
+});
