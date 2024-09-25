283ac151f8561ffec7c6386c9644127cdb361adb
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-25 20:15:48 +0200

    feat: Add features for extra credit
    The etsh a sketch is now rainbowy and cool!

--- script.js
+++ script.js
@@ -2,20 +2,36 @@ const container = document.querySelector("#container");
 
 function createGrid(size) {
   size = size || 16;
+  size = size > 100 ? 100 : size;
+
   const container = document.querySelector("#container");
   const gridElementSize = `${600 / size - 2}px`;
 
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("grid-element");
-    gridElement.addEventListener("mouseover", () => {
-      gridElement.classList.add("colored");
-    });
     gridElement.style.width = gridElementSize;
     gridElement.style.height = gridElementSize;
 
+    gridElement.addEventListener("mouseover", () => {
+      [...container.children].forEach(darken);
+      color(gridElement);
+    });
+
     container.appendChild(gridElement);
   }
+}
+
+function color(gridElement) {
+  const hueValue = Math.floor(Math.random() * 360);
+  const randomColor = `hsl(${hueValue}, 100%, 65%)`;
+  gridElement.style.backgroundColor = randomColor;
+  gridElement.style.opacity = "100%";
+}
+
+function darken(gridElement) {
+  console.log("darken");
+  gridElement.style.opacity = `${Number.parseFloat(gridElement.style.opacity) - 0.05}`;
 }
 
 function resetGame() {
